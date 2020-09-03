cb5c29bb4b4fae1c061e35f7e4e6f296c293c395
James <james.kiesel@gmail.com>
2020-09-03 22:06:35 +1200

    WIP more ical progress

--- functions/calendar.js
+++ functions/calendar.js
@@ -4,31 +4,36 @@ const generator = require('ical-generator')
 const moment = require('moment')
 const fs = require('fs')
 
-const dayPlacement = (weekCount, day, odd) => (
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    .filter(week => weekCount === 1 || (week % 2 === odd ? 1 : 0))
-    .map(week => moment().startOf('isoWeek').add(week, 'week').day(day))
-)
+const weeklyEvent = (event, ceremony, day, offset = 1) => {
+  const { startTime, endTime, weekCount } = ceremony
+  const m = moment().startOf('isoWeek').add(offset, 'week').day(day)
+
+  event.start = m.add(startTime, 'minutes')
+  event.end = m.add(endTime, 'minutes')
+  event.repeating({ freq: 'WEEKLY', interval: weekCount, count: 12 / weekCount })
+
+  return event
+}
 
 const eventPlacements = {
-  monday1: ({ weekCount }) => dayPlacement(weekCount, 'monday', true),
-  monday2: ({ weekCount }) => dayPlacement(weekCount, 'monday', false),
-  tuesday1: ({ weekCount }) => dayPlacement(weekCount, 'tuesday', true),
-  tuesday2: ({ weekCount }) => dayPlacement(weekCount, 'tuesday', false),
-  wednesday1: ({ weekCount }) => dayPlacement(weekCount, 'wednesday', true),
-  wednesday2: ({ weekCount }) => dayPlacement(weekCount, 'wednesday', false),
-  thursday1: ({ weekCount }) => dayPlacement(weekCount, 'thursday', true),
-  thursday2: ({ weekCount }) => dayPlacement(weekCount, 'thursday', false),
-  friday1: ({ weekCount }) => dayPlacement(weekCount, 'friday', true),
-  friday2: ({ weekCount }) => dayPlacement(weekCount, 'friday', false),
-  daily: () => (
-    ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
-      .map(day => dayPlacement(1, day))
-      .reduce((result, array) => result.concat(array), [])
-  ),
-  weekly: () => [], // TODO
-  monthly: () => [], // TODO
-  quarterly: () => [], // TODO
+  'monday-1': (event, ceremony) => weeklyEvent(event, ceremony, 'monday'),
+  'tuesday-1': (event, ceremony) => weeklyEvent(event, ceremony, 'tuesday'),
+  'wednesday-1': (event, ceremony) => weeklyEvent(event, ceremony, 'wednesday'),
+  'thursday-1': (event, ceremony) => weeklyEvent(event, ceremony, 'thursday'),
+  'friday-1': (event, ceremony) => weeklyEvent(event, ceremony, 'friday'),
+  'monday-2': (event, ceremony) => weeklyEvent(event, ceremony, 'monday', 2),
+  'tuesday-2': (event, ceremony) => weeklyEvent(event, ceremony, 'tuesday', 2),
+  'wednesday-2': (event, ceremony) => weeklyEvent(event, ceremony, 'wednesday', 2),
+  'thursday-2': (event, ceremony) => weeklyEvent(event, ceremony, 'thursday', 2),
+  'friday-2': (event, ceremony) => weeklyEvent(event, ceremony, 'friday', 2),
+  'daily': (event, ceremony) => {
+    const m = moment().startOf('isoWeek').add(1, 'week').day('monday')
+    event.start = m.add(ceremony.startTime)
+    event.end = m.add(ceremony.endTime)
+    event.repeating({ freq: 'DAILY', count: 30 })
+
+    return event
+  }
 }
 
 exports.upload = https.onRequest((req, res) => {
@@ -56,23 +61,18 @@ exports.upload = https.onRequest((req, res) => {
   })
 })
 
-const eventFromCeremony = (ical, room, { placement, uuid, startTime, endTime, notes, people = [] }) => {
-  console.log('wark', eventPlacements[placement](room), placement)
-  eventPlacements[placement](room).forEach(instance => {
-    console.log(instance)
-    const event = ical.createEvent({
-      start: instance.add(startTime, 'minutes'),
-      end: instance.add(endTime, 'minutes'),
-      timestamp: instance,
-      summary: notes
-    })
-
-    Object
-      .values(people)
-      .map(uuid => room.participants[uuid])
-      .filter(participant => participant)
-      .forEach(participant => attendeeFromParticipant(event, participant))
+const eventFromCeremony = (ical, room, ceremony) => {
+  const { uuid, placement, people = [] } = ceremony
+  const event = eventPlacements[placement](ical.createEvent(), {
+    ...ceremony,
+    weekCount: room.weekCount
   })
+
+  Object
+    .values(people)
+    .map(uuid => room.participants[ceremony.uuid])
+    .filter(participant => participant)
+    .forEach(participant => attendeeFromParticipant(event, participant))
 }
 
 const attendeeFromParticipant = (event, { email, username, optional }) => (
