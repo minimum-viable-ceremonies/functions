fa82cdeafc9479553290e6baca6a4730875ba360
James <james.kiesel@gmail.com>
2020-10-23 10:26:23 +1300

    Make MVC functions translatable

--- functions/mvc.js
+++ functions/mvc.js
@@ -1,11 +1,14 @@
 const { https, config } = require('firebase-functions')
 const cors = require('cors')
-const { createRoom } = require('./common')
+const i18n = require('i18next')
+const { createRoom, setLanguage } = require('./common')
 
 exports.create = https.onRequest((req, res) => (
-  cors({origin: config().mvc.cors_origin})(req, res, () => (
-    createRoom(JSON.parse(req.body)).errors
-      ? res.status(400).send({ status: 'bad request' })
-      : res.status(200).send({ status: 'ok' })
+  setLanguage(req).then(t => (
+    cors({origin: config().mvc.cors_origin})(req, res, () => (
+      createRoom(JSON.parse(req.body), t).errors
+        ? res.status(400).send({ status: t('common.errors.400') })
+        : res.status(200).send({ status: t('common.messages.200') })
+    ))
   ))
 ))
