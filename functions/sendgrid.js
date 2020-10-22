bd5b06d8c9198efcc63202c9d785a54885b2c083
James <james.kiesel@gmail.com>
2020-10-22 22:01:36 +1300

    WIP sendgrid send mail method

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -2,17 +2,36 @@ const axios = require('axios')
 const { https, config } = require('firebase-functions')
 const cors = require('cors')
 
-exports.subscribe = https.onRequest((req, res) => (
-  cors({origin: config().sendgrid.cors_origin})(req, res, () => (
+const headers = {
+  'Authorization': `Bearer ${config().sendgrid.api_key}`,
+  'Content-Type': 'application/json'
+}
+
+const respond = (origin, promise) =>
+  cors({ origin })(req, res, () =>
+    promise()
+      .then(() => res.status(200).send({status: 'ok'}))
+      .catch(({ request, response: { status, data } }) => res.status(status).send(data))
+  )
+
+exports.subscribe = https.onRequest((req, res) =>
+  respond(config().sendgrid.cors_origin, () =>
     axios.put('https://api.sendgrid.com/v3/marketing/contacts', {
       list_ids: [config().sendgrid.list_id],
       contacts: [{ email: req.body.email }]
-    }, {
-      headers: {
-        'Authorization': `Bearer ${config().sendgrid.api_key}`,
-        'Content-Type': 'application/json'
-      }
-    }).then(() => res.status(200).send({status: 'ok'}))
-      .catch(({ request, response: { status, data } }) => res.status(status).send(data))
-  ))
-))
+    }, { headers }))
+)
+
+exports.share = https.onRequest((req, res) =>
+  respond(config().mvc.cors_origin, () =>
+    axios.post('https://api.sendgrid.com/v3/mail/send', {
+      personalizations: [{
+        to: req.body.emails.map(email => ({ email }))
+      }],
+      from: { email: 'share@minimal.cards' },
+      content: [{
+        type: 'text/html',
+        value: '<div>Hello! Here is a minimum viable ceremony :)</div>'
+      }]
+    }, { headers }))
+)
